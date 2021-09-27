0cedeb6e5589071f35d4bf589357fda690bc560d
Micah Teeuws <tutmirage@gmail.com>
2021-09-27 11:40:19 +0300

    AccessFlags

--- structs.go
+++ structs.go
@@ -40,3 +40,31 @@ type CallSiteIdItem struct {
 
 type MethodHandleItem struct {
 }
+
+type AccessFlags uint64
+
+const (
+	ACC_PUBLIC       AccessFlags = 1 << iota
+	ACC_PRIVATE      AccessFlags = 1 << iota
+	ACC_PROTECTED    AccessFlags = 1 << iota
+	ACC_STATIC       AccessFlags = 1 << iota
+	ACC_FINAL        AccessFlags = 1 << iota
+	ACC_SYNCHRONIZED AccessFlags = 1 << iota
+	ACC_VOLATILE     AccessFlags = 1 << iota
+	ACC_TRANSIENT    AccessFlags = 1 << iota
+	ACC_NATIVE       AccessFlags = 1 << iota
+	ACC_INTERFACE    AccessFlags = 1 << iota
+	ACC_ABSTRACT     AccessFlags = 1 << iota
+	ACC_STRICT       AccessFlags = 1 << iota
+	ACC_SYNTHETIC    AccessFlags = 1 << iota
+	ACC_ANNOTATION   AccessFlags = 1 << iota
+	ACC_ENUM         AccessFlags = 1 << iota
+	_
+	ACC_CONSTRUCTOR           AccessFlags = 1 << iota
+	ACC_DECLARED_SYNCHRONIZED AccessFlags = 1 << iota
+)
+
+const (
+	ACC_BRIDGE  AccessFlags = ACC_VOLATILE
+	ACC_VARARGS AccessFlags = ACC_TRANSIENT
+)
