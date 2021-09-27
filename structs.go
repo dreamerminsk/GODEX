87b8d891a5344297277a86966f944cbb03c359d2
Micah Teeuws <tutmirage@gmail.com>
2021-09-27 11:25:14 +0300

    type DexFile struct

--- structs.go
+++ structs.go
@@ -1,1 +1,42 @@
 package godex
+
+type DexFile struct {
+	Header        *HeaderItem
+	StringIds     *[]StringIdItem
+	TypeIds       *[]TypeIdItem
+	ProtoIds      *[]ProtoIdItem
+	FieldIds      *[]FieldIdItem
+	MethodIds     *[]MethodIdItem
+	ClassDefs     *[]ClassDefItem
+	CallSiteIds   *[]CallSiteIdItem
+	MethodHandles *[]MethodHandleItem
+	Data          *[]uint8
+	LinkData      *[]uint8
+}
+
+type HeaderItem struct {
+}
+
+type StringIdItem struct {
+}
+
+type TypeIdItem struct {
+}
+
+type ProtoIdItem struct {
+}
+
+type FieldIdItem struct {
+}
+
+type MethodIdItem struct {
+}
+
+type ClassDefItem struct {
+}
+
+type CallSiteIdItem struct {
+}
+
+type MethodHandleItem struct {
+}
